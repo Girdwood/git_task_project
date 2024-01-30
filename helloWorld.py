2f6e11c2ebd335d0594e9cabd4ace8d8cccdf4ac
Chris Girdwood <chrisgirdwoood@gmail.com>
2024-01-30 23:37:12 +0000

    Added comments to the program for better documentation.

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
+# Prompt the user for their name, age, and favorite food
 user_name =input("What is your name?: ")
 user_age = input("What is your age?: ")
 user_fav_food = input("What is your favourite food?: ")
 
+# Display the gathered information
 print(f"This is {user_name}, they are {user_age} years old. Their favourite food is {user_fav_food}")
