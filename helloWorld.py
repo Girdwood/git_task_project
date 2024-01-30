5c9cfc9f46db9f5c45cceb8d379e08c606db85bd
Chris Girdwood <chrisgirdwoood@gmail.com>
2024-01-30 23:23:58 +0000

    Added more user input functions to collect more information from the user then displayed it using f strings.

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,5 @@
-user_input =input("Write anything here and it will display on the output ")
+user_name =input("What is your name?: ")
+user_age = input("What is your age?: ")
+user_fav_food = input("What is your favourite food?: ")
 
-print(user_input)
+print(f"This is {user_name}, they are {user_age} years old. Their favourite food is {user_fav_food}")
